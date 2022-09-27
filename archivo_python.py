91cda952756f8971f4da0866146a6adca9e5a257
blackSpider96 <86240928+blackSpider96@users.noreply.github.com>
2022-09-27 14:13:29 -0600

    se agregan mangas

--- archivo_python.py
+++ archivo_python.py
@@ -22,42 +22,12 @@ class Animalitos():
 
 
 
-class TipoComponente(Enum):
-    COMENTARIO      = auto()
-    TEXTO           = auto()
-    DOSPUNTOS       = auto()
-    ESPACIO         = auto()
-    PUNTUACION      = auto()
-    COMILLA         = auto()
-    
-    INICIOCUENTO    = auto()
-    FECHAPUBLICACION= auto()
-    AUTOR           = auto()
-    TITULO          = auto()
-    TIPOCUENTO      = auto()
-
-    NEGRITA         = auto()
-    ITALICA         = auto()
-    TAMANOLETRA     = auto()
-
-    CONDICIONAL     = auto()
-    REPETICION      = auto()
-    # NUMEROS         = auto()
-    ENTEROS         = auto()
-
-    VAR             = auto()
-    FUNCION         = auto()
-    IDENTIFICADOR   = auto()
-
-    OPERADORES      = auto()
-    SALTOLINEA      = auto()
-    TABULAR         = auto()
-    ESPACIOS        = auto()
-
-    FINCUENTO       = auto()
-    ERROR           = auto()
 
 
+class Manga():
+    Naruto
+    One Piece
+    Dragon ball
 
 
 class Explorador:
