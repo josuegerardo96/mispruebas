000e7e0767698c6cb5ec2e15f24c7e88f8092015
blackSpider96 <86240928+blackSpider96@users.noreply.github.com>
2022-09-27 13:47:51 -0600

    Se agrega a archivo_python los elementos faltantes

--- archivo_python.py
+++ archivo_python.py
@@ -3,6 +3,52 @@ import re                   # Para expresiones regulares
 from texttable import Texttable
 
 
+class TipoComponente(Enum):
+    COMENTARIO      = auto()
+    TEXTO           = auto()
+    DOSPUNTOS       = auto()
+    ESPACIO         = auto()
+    PUNTUACION      = auto()
+    COMILLA         = auto()
+    
+    INICIOCUENTO    = auto()
+    FECHAPUBLICACION= auto()
+    AUTOR           = auto()
+    TITULO          = auto()
+    TIPOCUENTO      = auto()
+
+    INTRODUCCION    = auto()
+    DESARROLLO      = auto()
+    CONCLUSION      = auto()
+    ABRECORCHETE    = auto()
+    CIERRACORCHETE  = auto()
+    ABREPARENTESIS  = auto()
+    CIERRAPARENTESIS= auto()
+
+    NEGRITA         = auto()
+    ITALICA         = auto()
+    TAMANOLETRA     = auto()
+
+    CONDICIONAL     = auto()
+    REPETICION      = auto()
+    # NUMEROS         = auto()
+    ENTEROS         = auto()
+
+    VAR             = auto()
+    FUNCION         = auto()
+    IDENTIFICADOR   = auto()
+
+    OPERADORES      = auto()
+    SALTOLINEA      = auto()
+    TABULAR         = auto()
+    ESPACIOS        = auto()
+
+    FINCUENTO       = auto()
+    ERROR           = auto()
+
+
+
+
 class ComponenteLexico:
     
     tipo : TipoComponente
